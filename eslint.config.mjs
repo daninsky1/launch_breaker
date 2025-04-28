e3634963789ee2dbdc2641c5a168373d65a504bb
Daninsky <daninsky12@gmail.com>
2025-04-28 16:53:53 -0300

    eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import globals from "globals";
+import {defineConfig} from "eslint/config";
+
+
+export default defineConfig([
+    {files: ["**/*.{js,mjs,cjs}"], plugins: {js}, extends: ["js/recommended"]},
+    {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+    {files: ["**/*.{js,mjs,cjs}"], languageOptions: {globals: globals.browser}},
+    {
+        rules: {
+            semi: ["error", "always"],
+            "no-var": "error",
+            "no-unused-vars": "off",
+            "no-global-assign": "off",
+            "guard-for-in": "error",
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    vars: "all",
+                    args: "after-used",
+                    ignoreRestSiblings: true,
+                },
+            ],
+            "prefer-const": [
+                "error",
+                {
+                    destructuring: "all",
+                },
+            ],
+            "node/no-callback-literal": "off",
+            "node/no-deprecated-api": "off",
+        },
+    },
+]);
